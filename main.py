0756ef20d87dde9eb7afc25b2eb010fa418cb375
Adrion Merecer <dooneymercer5@gmail.com>
2026-09-21 09:07:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,23 @@ print("       MUSIC FAN PERSONALITY QUIZ")
 print("====================================")
 print("Answer the questions to find out what type of music fan you are!")
 print()
+
+# Question 1
+print("Question 1: What type of music do you listen to the most?")
+print("1. Hip-Hop/Rap")
+print("2. R&B")
+print("3. Pop")
+print("4. A little bit of everything")
+
+answer1 = input("Enter 1, 2, 3, or 4: ")
+
+if answer1 == "1":
+    hip_hop += 1
+elif answer1 == "2":
+    rnb += 1
+elif answer1 == "3":
+    pop += 1
+else:
+    hip_hop += 1
+    rnb += 1
+    pop += 1
